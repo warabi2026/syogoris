1097021e42e5756f956f9e20ed3d900102fc5247
warabi2026 <office@warabi.link>
2026-03-30 21:59:18 +0900

    改善４

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* テトリス PWA — オフライン起動用の最小キャッシュ */
-const CACHE_NAME = "tetris-pwa-v2";
+const CACHE_NAME = "tetris-pwa-v3";
 const PRECACHE_URLS = ["./index.html", "./manifest.json", "./icon.svg"];
 
 self.addEventListener("install", function (e) {
